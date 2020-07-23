91f682f8b15e30eadfccb03d1285c3b88318441d
ivan san <sii200192@gmail.com>
2020-07-23 13:37:30 +0700

    Добавлен react, react-dom, создан первый компопнент

--- homework/1_introduction/src/index.js
+++ homework/1_introduction/src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+class Hello extends React.Component{
+    render() {
+        const name = this.props.name;
+
+        return <div>Hello, {name}</div>;
+    }
+}
+
+ReactDOM.render(
+    <Hello name='Ivan' />,
+    document.getElementById('root'),
+);
