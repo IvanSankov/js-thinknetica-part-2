c7a2b5f35691bde25593677d7d7083f9426b5054
ivan san <sii200192@gmail.com>
2020-07-25 23:47:04 +0700

    Небольшие космитологические улучшения

--- homework/1_introduction/src/components/Author.js
+++ homework/1_introduction/src/components/Author.js
@@ -12,12 +12,18 @@ export default class Author extends React.Component {
     return (
       <div className='row'>
         <h3>Author</h3>
-        <div className="col-sm-12">
-          <img src={author.avatar} className='img-fluid' alt={author.name}/>
+        <div className="row">
+          <div className="col-sm-4">
+            <img src={author.avatar} className='img-fluid' alt={author.name}/>
+          </div>
+          <div className="col-sm-8">
+            <div className="row">
+              <div className='col-sm-12'><b>Name</b>: {author.name}</div>
+              <div className='col-sm-12'><b>Email</b>: {author.email}</div>
+              <div className='col-sm-12'><b>Bio</b>: {author.bio}</div>
+            </div>
+          </div>
         </div>
-        <div className='col-sm-12'><b>Name</b>: {author.name}</div>
-        <div className='col-sm-12'><b>Email</b>: {author.email}</div>
-        <div className='col-sm-12'><b>Bio</b>: {author.bio}</div>
       </div>
     );
   }
