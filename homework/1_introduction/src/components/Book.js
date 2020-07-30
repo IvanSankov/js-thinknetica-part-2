bdd62bacb9ae80ba2e92b2146318d0cc9f130673
ivan san <sii200192@gmail.com>
2020-07-30 23:48:21 +0700

    Добавлена форма обратной связи

--- homework/1_introduction/src/components/Book.js
+++ homework/1_introduction/src/components/Book.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import AuthorList from './AuthorList'
 import BookInfo from "./BookInfo";
+import ContactForm from "./ContactForm";
 import EmptyBlock from "./EmptyBlock";
 
 const MIN_SUBSCRIBERS_FOR_POPULARITY = 100;
@@ -10,22 +11,27 @@ export default class Book extends React.Component {
   render() {
     const {book} = this.props;
     if (!book) {
-      return <EmptyBlock />;
+      return <EmptyBlock/>;
     }
 
     const popular = book.subscribers > MIN_SUBSCRIBERS_FOR_POPULARITY
-        && <span className='badge badge-primary'>Popular</span>;
+      && <span className='badge badge-primary'>Popular</span>;
 
     return (
-      <div className='row bg-light'>
-        <div className='col-sm-7'>
-          <h3>Book {popular}</h3>
-          <BookInfo book={book} />
+      <>
+        <div className='row bg-light'>
+          <div className='col-sm-7'>
+            <h3>Book {popular}</h3>
+            <BookInfo book={book}/>
+          </div>
+          <div className='col-sm-4'>
+            <AuthorList authors={book.authors}/>
+          </div>
+          <div className='col-sm-12'>
+            <ContactForm/>
+          </div>
         </div>
-        <div className='col-sm-4'>
-          <AuthorList authors={book.authors}/>
-        </div>
-      </div>
+      </>
     );
   }
 }
--- homework/1_introduction/src/components/BookInfo.js
+++ homework/1_introduction/src/components/BookInfo.js
@@ -25,10 +25,10 @@ export default class BookInfo extends React.Component {
             <div className='col-sm-12'><b>Pages</b>: {book.pages}</div>
             <div className='col-sm-12'><b>Language</b>: {book.language}</div>
             <div className='col-sm-12'><b>Progress</b>: {book.progress}%</div>
-            <div className='col-sm-12'><b>Min price</b>: {book.minPrice}$</div>
-            <div className='col-sm-12'><b>Desired price</b>: {book.desiredPrice}$</div>
-            <div className='col-sm-12'><b>Current sum</b>: {book.currentSum}$</div>
-            <div className='col-sm-12'><b>Expected sum</b>: {book.expectedSum}$</div>
+            <div className='col-sm-12'><b>Min price</b>: ${book.minPrice}</div>
+            <div className='col-sm-12'><b>Desired price</b>: ${book.desiredPrice}</div>
+            <div className='col-sm-12'><b>Current sum</b>: ${book.currentSum}</div>
+            <div className='col-sm-12'><b>Expected sum</b>: ${book.expectedSum}</div>
           </div>
         </div>
       </div>
